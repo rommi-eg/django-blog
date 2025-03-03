8cd9514ae1dbbe9a0efb25e064362460a433e3af
rommieg <_csg@mail.ru>
2025-03-03 08:53:09 +0300

    Добавлено представление списка постов

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    """ Представление списка постов на странице """
+    posts = Post.published.all()
+    return render(
+        request=request,
+        template_name='blog/post/list.html',
+        context={'posts': posts},
+    )
