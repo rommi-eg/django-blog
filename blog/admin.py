ef1232caec9bf2788e80bd908c80cc194c909fd9
rommieg <_csg@mail.ru>
2025-03-03 07:21:25 +0300

    Изменен способ регистрации модели

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post) # регистрирует модель в панели администратора
+class PostAdmin(admin.ModelAdmin):
+    """ Класс внешнего вида модели в панели администратора """
+
+    # Позволяет задавать поля модели, которые отобразятся на
+    # странице списка объектов администрирования.
+    list_display = (  
+        'title', 'slug', 'author', 'publish', 'status',
+    )
