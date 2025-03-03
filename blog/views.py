6e18bfc27f61d95c875c26c2bf79e282c22deff3
rommieg <_csg@mail.ru>
2025-03-03 09:02:57 +0300

    Добавлены комментарии к представлению post_list

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,11 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     """ Представление списка постов на странице """
-    posts = Post.published.all()
+
+    # Извлекаем все посты со статусом PUBLISHED
+    posts = Post.published.all() # ипользуется созданый ранее менеджер
     return render(
-        request=request,
-        template_name='blog/post/list.html',
-        context={'posts': posts},
+        request=request, # принимаемый объект request
+        template_name='blog/post/list.html', # путь к шаблону
+        context={'posts': posts}, # контекстные переменные, чтобы прорисовать даннный щаблон
     )
