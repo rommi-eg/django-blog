4899ab7df22ee4ebf7d8e32e5878e67f8f41c682
rommieg <_csg@mail.ru>
2025-03-04 07:28:55 +0300

    Изменили шаблон url-адресов в приложении blog

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,8 @@ app_name = 'blog' # пространство имен приложения blog
 urlpatterns = [
     # представление поста
     path('', view=views.post_list, name='post_list'),
-    path('<int:id>/', view=views.post_detail, name='post_detail'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>',
+         view=views.post_detail, name='post_detail'
+    ),
 ]
