ca6f1d03e7d5434a70d66df01902d63ee83cc544
rommieg <_csg@mail.ru>
2025-03-14 10:17:06 +0300

    Добавлены представления post_list и post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    """ Представление списка постов на странице """
+
+    # Извлекаем все посты со статусом PUBLISHED
+    # используя созданый ранее менеджер
+    posts = Post.published.all()
+
+    # Контекстные переменные, чтобы прорисовать шаблон
+    context = {
+        'posts': posts,
+    }
+
+    # Шаблон в котором прорисовывается контекст
+    template = 'blog/post/list.html'
+
+    return render(request=request, template_name=template, context=context)
+
+
+def post_detail(request, id):
+    """ Представление одиночного поста на странице """
+
+    # Извлекаем объект, соответствующий переданным параметрам.
+    # Если объект не найден вернется мсключение HTTP с кодом
+    # состояния 404.
+    post = get_object_or_404(
+        Post, 
+        id=id, 
+        status=Post.Status.PUBLISHED,
+    )
+
+    # Контекстные переменные, чтобы прорисовать шаблон
+    context = {
+        'post': post,
+    }
+
+    # Шаблон в котором прорисовывается контекст
+    template = 'blog/post/detail.html'
+    
+    return render(request=request, template_name=template, context=context)
