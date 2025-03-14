91138664715bfadbc168a1f438eb65de4f325d1b
rommieg <_csg@mail.ru>
2025-03-14 12:49:40 +0300

    Добавлен метод get_absolute_url в модели Post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 
@@ -105,3 +106,7 @@ class Post(models.Model):
         """ Возвращает строковый литерал объекта в удобочитаемом представлении """
         return self.title
     
+    def get_absolute_url(self):
+        """ Возвращает канонический url-адрес объекта """
+        return reverse('blog:post_detail', args=[self.id])
+
