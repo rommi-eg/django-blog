8761e0f8c60600f64ea42d3d7b5e0a5b3e8b26ef
rommieg <_csg@mail.ru>
2025-03-16 08:02:44 +0300

    Добавлены шаблоны для отправки комментария

--- blog/urls.py
+++ blog/urls.py
@@ -16,8 +16,9 @@ urlpatterns = [
     # PostListView
     path('', view=views.PostListView.as_view(), name='post_list'),
 
-    # Шаблон url-адреса, который не принимает один аргумент
+    # Шаблон url-адреса, который принимает один аргумент
     # id и соотносится с представлением post_detail
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', view=views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', view=views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', view=views.post_comment, name='post_comment'),
 ]
