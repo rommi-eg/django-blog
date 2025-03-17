e9682a777fcce3db4633292d834278840eb40169
rommieg <_csg@mail.ru>
2025-03-17 19:43:44 +0300

    Добавлено представление поиска по постам

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,10 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
 
@@ -208,5 +209,27 @@ def post_comment(request, post_id):
     context = {'post': post, 'form': form, 'comment': comment}
 
     template = 'blog/post/comment.html'
+
+    return render(request=request, template_name=template, context=context)
+
+
+def post_search(request):
+    """ Представление для поиска """
+
+    form = SearchForm()
+    query = None
+    result = list()
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid:
+            query = form.cleaned_data['query']
+            result = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    
+    context = {'form': form, 'query': query, 'result': result}
+
+    template = 'blog/post/search.html'
 
     return render(request=request, template_name=template, context=context)
